393e4ab9bd8fd7a4f7a00738da440e884d130f6f
Suman Kumari <sumankumari200603@gmail.com>
2026-03-14 00:49:42 +0530

    save

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+function App() {
+ 
+  return (
+    <div>
+      <Navbar />
+      <Card />
+      <Card />
+      
+      
+    </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div className="card">
+        <h1>suman</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, corporis.</p>
+    </div>
+  )
+}
+
+export default card
