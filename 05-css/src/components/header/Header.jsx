2122a0d52acdd8b0b635ffcd1bdc93a97a938e51
Suman Kumari <sumankumari200603@gmail.com>
2026-03-17 23:42:15 +0530

    save

---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header.module.css'
+const Header = () => {
+  return (
+    <div className={styles.header}>
+        <h1>shreyans</h1>
+        <button>login</button>
+    </div>
+  )
+}
+
+export default Header
